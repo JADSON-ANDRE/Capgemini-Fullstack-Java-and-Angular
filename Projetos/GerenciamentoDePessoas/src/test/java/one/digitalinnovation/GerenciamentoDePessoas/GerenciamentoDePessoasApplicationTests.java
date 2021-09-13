918b3a6f95c45ef921de31fc38aa920d801649da
JADSON-ANDRE <jadsoncic@gmail.com>
2021-09-13 09:57:22 -0300

    Subindo configurações iniciais do Projeto API Rest com Spring

---
+++ Projetos/GerenciamentoDePessoas/src/test/java/one/digitalinnovation/GerenciamentoDePessoas/GerenciamentoDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.GerenciamentoDePessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
